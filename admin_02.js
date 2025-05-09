6bab43b2230160eadab341592d86a67cf8f30631
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2025-05-09 09:16:07 +0300

    Update admin_02.js

--- admin_02.js
+++ admin_02.js
@@ -1,19 +1,19 @@
-document.addEventListener('', () => {
   const createModal = document.getElementById('create-booking-modal');
   const openCreateBtn = document.getElementById('open-create-modal');
   const closeCreateBtn = document.getElementById('close-create-modal');
 
   openCreateBtn.addEventListener('click', () => {
-    createModal.style.display="block";
+    createModal.style.display = 'block';
   });
 
   closeCreateBtn.addEventListener('click', () => {
-    createModal.style.display="none";
+    createModal.style.display = 'none';
   });
 
   window.addEventListener('click', (e) => {
     if (e.target === createModal) {
-      createModal.classList.add('hidden');
+      createModal.style.display = 'none';
     }
   });
-});
+
+
